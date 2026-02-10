c891412619ed5d47b0f86a8ea29eaf83489f9621
HARSH ANAND <harshanand@HARSHs-MacBook-Air.local>
2026-02-10 13:20:15 +0530

    UI Final Fix: Adjusted Vite base path to /dist/ for correct asset resolution and seamless blending

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+    base: "/dist/",
     plugins: [
+
         react(),
         tailwindcss(),
     ],
