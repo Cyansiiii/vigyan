3679bdb11354a94388db1a89310154a73746d85d
HARSH ANAND <harshanand@HARSHs-MacBook-Air.local>
2026-03-12 16:07:05 +0530

    feat: refine organic login design with complex curbs and fix asset deployment

--- deploy_ui.sh
+++ deploy_ui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 expect -c '
 set timeout 60
-spawn scp -P 65002 -r "frontend/js/view-questions.js" "frontend/js/admin-utils.js" "signinpage.html" u255161845@31.97.101.169:domains/vigyanprep.com/public_html/
+spawn scp -P 65002 -r "frontend/js/view-questions.js" "frontend/js/admin-utils.js" "signinpage.html" "assets/" u255161845@31.97.101.169:domains/vigyanprep.com/public_html/
 expect "password:"
 send "Buddy700@\r"
 expect eof
